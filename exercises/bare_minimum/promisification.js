9f3fcc7c16d7fa452f368eb8088fb67c9ceba232
Owen Yoshishige <owenyoshishige@gmail.com>
2022-03-15 16:21:48 -0700

    complete bare_minimum/promisification.js excersis

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -1,12 +1,16 @@
 /**
  * Create the promise returning `Async` suffixed versions of the functions below,
  * Promisify them if you can, otherwise roll your own promise returning function
- */ 
+ */
 
 var fs = require('fs');
 var request = require('needle');
 var crypto = require('crypto');
 var Promise = require('bluebird');
+var nodeStyle = require('./callbackReview.js');
+var pluckFirstLineFromFileAsync = Promise.promisify(nodeStyle.pluckFirstLineFromFile);
+var getStatusCodeAsync = Promise.promisify(nodeStyle.getStatusCode);
+// Promise.promisifyAll(fs);
 
 // (1) Asyncronous HTTP request
 var getGitHubProfile = function (user, callback) {
@@ -29,7 +33,7 @@ var getGitHubProfile = function (user, callback) {
   });
 };
 
-var getGitHubProfileAsync; // TODO
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
 
 
 // (2) Asyncronous token generation
@@ -40,25 +44,25 @@ var generateRandomToken = function(callback) {
   });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken);
 
 
 // (3) Asyncronous file manipulation
 var readFileAndMakeItFunny = function(filePath, callback) {
   fs.readFile(filePath, 'utf8', function(err, file) {
     if (err) { return callback(err); }
-   
+
     var funnyFile = file.split('\n')
       .map(function(line) {
         return line + ' lol';
       })
       .join('\n');
 
-    callback(funnyFile);
+    callback(null, funnyFile);
   });
 };
 
-var readFileAndMakeItFunnyAsync; // TODO
+var readFileAndMakeItFunnyAsync = Promise.promisify(readFileAndMakeItFunny);
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
