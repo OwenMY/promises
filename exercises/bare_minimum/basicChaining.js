f9a96c7bdb280be38508f428c473fe7c223193d4
Owen Yoshishige <owenyoshishige@gmail.com>
2022-03-15 21:01:34 -0700

    Complete BMR's for promises sprint

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,12 +10,50 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
+var request = require('needle');
+var {getGitHubProfileAsync, generateRandomTokenAsync, readFileAndMakeItFunnyAsync} = require('./promisification.js');
+var {getStatusCodeAsync, pluckFirstLineFromFileAsync} = require('./promiseConstructor.js');
+Promise.promisifyAll(fs);
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return pluckFirstLineFromFileAsync(readFilePath)
+    .then((handle) => getGitHubProfileAsync(handle))
+    .then((message) => fs.writeFileAsync(writeFilePath, JSON.stringify(message)));
 };
+//   return fs.readFileAsync(readFilePath).then((data) => {
+//     let fileData = '' + data;
+//     let indexN = fileData.indexOf('\n');
+//     let githubHandle = fileData.slice(0, indexN);
+//     return githubHandle;
+//   })
+//     .then((githubH) => {
+//       request('get', 'https://api.github.com/users/' + githubH)
+//         .then((response) => {
+//           // console.log(response.statusCode, 'this is the STATUS');
+//           // console.log(response.body.id);
+//           let userInfo = {
+//             id: response.body.id,
+//             login: response.body.login,
+//             name: response.body.name,
+//             company: response.body.company,
+//             location: response.body.location};
+//             console.log(userInfo);
+//             console.log(writeFilePath);
+
+//           fs.writeFileAsync(writeFilePath, 'utf8', JSON.stringify(userInfo))
+//             .then((err) => {
+//               return 'error';
+//             });
+//         })
+//         .catch((error) => {
+//           console.error(error);
+//         });
+
+//     });
+
+// };
+
 
 // Export these functions so we can test them
 module.exports = {
